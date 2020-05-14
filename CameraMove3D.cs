176e802a498906af6d738f95e12127099633ad42
FluxO4 <46083008+FluxO4@users.noreply.github.com>
2020-05-14 19:03:29 +0100

    Finished 3D

--- CameraMove3D.cs
+++ CameraMove3D.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class CameraMove3D : MonoBehaviour
 {
-    [Range(0,3)]
-    public float playerRotationDelay = 1.0f; //Set to 0 for instant player rotation
+    [Range(0.05f,1f)]
+    public float playerRotationDelay = 0.3f; //Set to 1 for instant player rotation
+
+    [Range(10, 90)]
+    public int LookDownAngleLimit = 80;
+
+    [Range(10, 90)]
+    public int LookUpAngleLimit = 80;
 
     public GameObject playerRoot;
     public GameObject playerModel;
@@ -32,16 +38,18 @@ public class CameraMove3D : MonoBehaviour
         float v = -5 * Input.GetAxis("Mouse Y");
 
         playerRoot.transform.Rotate(playerRoot.transform.up, h * Time.deltaTime * 20, Space.Self);
-        
+        playerModel.transform.Rotate(playerRoot.transform.up, -h * Time.deltaTime * 20, Space.Self);
 
-        if (head.transform.localEulerAngles.x + v * Time.deltaTime * 20 > 70 && head.transform.localEulerAngles.x + v * Time.deltaTime * 20 <= 180)
+        playerModel.transform.Rotate(playerRoot.transform.up, -Vector3.SignedAngle(playerRoot.transform.forward, playerModel.transform.forward, playerRoot.transform.up)*playerRotationDelay, Space.Self);
+
+        if (head.transform.localEulerAngles.x + v * Time.deltaTime * 20 > LookDownAngleLimit && head.transform.localEulerAngles.x + v * Time.deltaTime * 20 <= 180)
         {
-            head.transform.localEulerAngles = new Vector3(70, head.transform.localEulerAngles.y, head.transform.localEulerAngles.z);
+            head.transform.localEulerAngles = new Vector3(LookDownAngleLimit, head.transform.localEulerAngles.y, head.transform.localEulerAngles.z);
         }
         else
-        if (head.transform.localEulerAngles.x + v * Time.deltaTime * 20 < 280 && head.transform.localEulerAngles.x + v * Time.deltaTime * 20 > 180)
+        if (head.transform.localEulerAngles.x + v * Time.deltaTime * 20 < (360- LookUpAngleLimit) && head.transform.localEulerAngles.x + v * Time.deltaTime * 20 > 180)
         {
-            head.transform.localEulerAngles = new Vector3(280, head.transform.localEulerAngles.y, head.transform.localEulerAngles.z);
+            head.transform.localEulerAngles = new Vector3((360 - LookUpAngleLimit), head.transform.localEulerAngles.y, head.transform.localEulerAngles.z);
         }
         else
         {
